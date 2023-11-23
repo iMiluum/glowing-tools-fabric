af6a33319101815d66b6d761d381d729f7474939
Miluum <imiluum@gmail.com>
2023-11-23 13:57:30 +0100

    feat: added 1.20.2 support

--- src/client/java/com/miluum/glowing_tools/GlowingToolsClient.java
+++ src/client/java/com/miluum/glowing_tools/GlowingToolsClient.java
@@ -1,10 +1,6 @@
 package com.miluum.glowing_tools;
 
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
-import net.minecraft.item.Item;
-import net.minecraft.util.Identifier;
-import net.minecraft.util.registry.Registry;
 
 public class GlowingToolsClient implements ClientModInitializer {
 
