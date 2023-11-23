a478f111652b158dd15ffadd2ef61537dafb7618
Miluum <imiluum@gmail.com>
2023-11-23 14:42:21 +0100

    wip: adding 1.20.1 support

---
+++ remappedSrc/com/miluum/glowing_tools/GlowingShovelItem.java
@@ -0,0 +1,23 @@
+package com.miluum.glowing_tools;
+
+import net.minecraft.client.item.TooltipContext;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ShovelItem;
+import net.minecraft.item.ToolMaterial;
+import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
+import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+
+public class GlowingShovelItem extends ShovelItem {
+    public GlowingShovelItem(ToolMaterial material, float attackDamage, float attackSpeed, Settings settings) {
+        super(material, attackDamage, attackSpeed, settings);
+    }
+
+    @Override
+    public void appendTooltip(ItemStack stack, @Nullable World world, List<Text> tooltip, TooltipContext context) {
+        tooltip.add(Text.translatable("item.glowing_tools.default.tooltip").formatted(Formatting.GRAY));
+    }
+}
---
+++ remappedSrc/com/miluum/glowing_tools/GlowingSwordItem.java
@@ -0,0 +1,23 @@
+package com.miluum.glowing_tools;
+
+import net.minecraft.client.item.TooltipContext;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.SwordItem;
+import net.minecraft.item.ToolMaterial;
+import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
+import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+
+public class GlowingSwordItem extends SwordItem {
+    public GlowingSwordItem(ToolMaterial material, int attackDamage, float attackSpeed, Settings settings) {
+        super(material, attackDamage, attackSpeed, settings);
+    }
+
+    @Override
+    public void appendTooltip(ItemStack stack, @Nullable World world, List<Text> tooltip, TooltipContext context) {
+        tooltip.add(Text.translatable("item.glowing_tools.default.tooltip").formatted(Formatting.GRAY));
+    }
+}
