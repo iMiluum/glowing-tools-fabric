8637003fcf44801ad6ea60a9459343d8cfa61328
Miluum <imiluum@gmail.com>
2023-11-25 00:38:15 +0100

    feat: place and pick up torches

--- src/main/java/com/miluum/glowing_tools/GlowingAxeItem.java
+++ src/main/java/com/miluum/glowing_tools/GlowingAxeItem.java
@@ -7,6 +7,7 @@ import net.minecraft.enchantment.Enchantment;
 import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.*;
 import net.minecraft.nbt.NbtList;
 import net.minecraft.state.property.Properties;
@@ -25,12 +26,9 @@ import org.slf4j.Logger;
 import java.util.List;
 
 public class GlowingAxeItem extends AxeItem implements GlowingItem{
-    public GlowingAxeItem(ToolMaterial material, float attackDamage, float attackSpeed, Settings settings, Logger LOGGER) {
+    public GlowingAxeItem(ToolMaterial material, float attackDamage, float attackSpeed, Settings settings) {
         super(material, attackDamage, attackSpeed, settings);
-        this.LOGGER = LOGGER;
     }
-
-    private final Logger LOGGER;
 
     @Override
     public void appendTooltip(ItemStack stack, @Nullable World world, List<Text> tooltip, TooltipContext context) {
@@ -38,83 +36,22 @@ public class GlowingAxeItem extends AxeItem implements GlowingItem{
     }
 
     @Override
-    public boolean postMine(ItemStack stack, World world, BlockState state, BlockPos pos, LivingEntity miner) {
-        if (!world.isClient && state.getHardness(world, pos) != 0.0F) {
-            stack.damage(1, miner, (e) -> {
-                e.sendEquipmentBreakStatus(EquipmentSlot.MAINHAND);
-            });
-        }
-        return true;
-    }
-
-    @Override
     public ActionResult useOnBlock(ItemUsageContext context) {
         ItemStack stack = context.getStack();
         World world = context.getWorld();
         BlockPos pos = context.getBlockPos();
-        LivingEntity miner = context.getPlayer();
+        PlayerEntity miner = context.getPlayer();
         Direction side = context.getSide();
 
-        if (isGlowing(stack)) {
-            placeTorch(world, pos, side);
-            turnToNormalAxe(stack, miner);
-        }
+
+        assert miner != null;
+        if (miner.isSneaking()) GlowingItemTransformer.transformGlowingTool(stack, world, pos, miner, side);
+        else super.useOnBlock(context);
+
         return ActionResult.SUCCESS;
     }
 
     public boolean isGlowing(ItemStack stack) {
-        return stack.getItem() instanceof GlowingAxeItem;
+        return stack.getItem() instanceof GlowingItem;
     }
-
-    private void placeTorch(World world, BlockPos pos, Direction side) {
-        BlockState torchState;
-        if (side == Direction.UP) {
-            torchState = Blocks.TORCH.getDefaultState();
-        }
-        else if (side == Direction.DOWN) {
-            torchState = Blocks.WALL_TORCH.getDefaultState();
-        }
-        else {
-            torchState = Blocks.WALL_TORCH.getDefaultState().with(Properties.HORIZONTAL_FACING, side);
-        }
-        BlockPos torchPos = pos.offset(side);
-
-        world.setBlockState(torchPos, torchState);
-    }
-
-    private ItemStack getVanillaTool(ItemStack stack){
-        AxeItem normalAxe;
-        if (getMaterial().equals(ToolMaterials.WOOD))
-            normalAxe = (AxeItem) Items.WOODEN_AXE;
-        else if (getMaterial().equals(ToolMaterials.STONE))
-            normalAxe = (AxeItem) Items.STONE_AXE;
-        else if (getMaterial().equals(ToolMaterials.IRON))
-            normalAxe = (AxeItem) Items.IRON_AXE;
-        else if (getMaterial().equals(ToolMaterials.GOLD))
-            normalAxe = (AxeItem) Items.GOLDEN_AXE;
-        else if (getMaterial().equals(ToolMaterials.DIAMOND))
-            normalAxe = (AxeItem) Items.DIAMOND_AXE;
-        else if (getMaterial().equals(ToolMaterials.NETHERITE))
-            normalAxe = (AxeItem) Items.NETHERITE_AXE;
-        else
-            normalAxe = (AxeItem) Items.WOODEN_AXE;
-        return new ItemStack(normalAxe);
-    }
-
-    private void copyData(ItemStack stack, ItemStack normalAxeStack) {
-        EnchantmentHelper.set(EnchantmentHelper.get(stack), normalAxeStack);
-    }
-
-    private void turnToNormalAxe(ItemStack stack, LivingEntity miner) {
-        try {
-            ItemStack normalAxeStack = getVanillaTool(stack);
-            copyData(stack, normalAxeStack);
-            stack.decrement(1);
-            miner.equipStack(EquipmentSlot.MAINHAND, normalAxeStack);
-        }
-        catch (Exception e) {
-            LOGGER.error("E: " + e);
-        }
-    }
-
 }
