9df6b97caec51a065b16991a6ee2c4c82f0f9a69
Miluum <imiluum@gmail.com>
2023-11-27 13:41:39 +0100

    fix: fixed shields not working

--- src/main/java/com/miluum/glowing_tools/GlowingAxeItem.java
+++ src/main/java/com/miluum/glowing_tools/GlowingAxeItem.java
@@ -48,7 +48,7 @@ public class GlowingAxeItem extends AxeItem implements GlowingItem{
 
         assert miner != null;
         if (miner.isSneaking()) GlowingItemTransformer.transformGlowingTool(stack, world, pos, miner, side);
-        else super.useOnBlock(context);
+        else return super.useOnBlock(context);
 
         return ActionResult.SUCCESS;
     }
